5d26f3da5ecb520da0c7afd923f7b5cd2231c60d
viktoriias <viktoriias@users.noreply.github.com>
2017-02-10 20:54:06 -0800

    Add files via upload

---
+++ Ex22_1.java
@@ -0,0 +1,13 @@
+
+public class Ex22_1 {
+	public static void main(String[] args) {
+//		String s = "abcabcdgabxy"; 
+		String s = "abcabcdgabmnsxy"; 
+		
+		for(int i = 0; i < 5; i++){
+			for(int j = 0; j < 10; j++)
+			System.out.printf("%-2c", '∏');
+			System.out.println();
+		}
+	}
+}
